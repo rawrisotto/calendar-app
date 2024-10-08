2fedaeaa0b82579680eba0e8761ab398543915b8
Hidayat <kudahidayat8@gmail.com>
2024-10-08 23:12:35 +0800

    Added add event button

--- components/Events.jsx
+++ components/Events.jsx
@@ -3,9 +3,18 @@ import { format } from "date-fns";
 
 const Events = ({ selectedDate }) => {
   return (
-    <aside>
-      <h3 className="text-xl font-semibold">{format(selectedDate, "eeee")}</h3>
-      <h2 className="text-2xl font-semibold">{format(selectedDate, "MMMM d")}</h2>
+    <aside className="grid content-between">
+      <div>
+        <h3 className="text-xl font-semibold">
+          {format(selectedDate, "eeee")}
+        </h3>
+        <h2 className="text-2xl font-semibold">
+          {format(selectedDate, "MMMM d")}
+        </h2>
+      </div>
+      <button className="btn border-blue-100 bg-blue-100 hover:bg-blue-400 hover:text-neutral-100">
+        Add Event
+      </button>
     </aside>
   );
 };
